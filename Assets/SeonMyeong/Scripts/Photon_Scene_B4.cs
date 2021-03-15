15d538b02034b7adc60a9b4a8e19eaa3ff808890
jabtem <suchie777@gmail.com>
2021-03-15 12:30:46 +0900

    B4한줄수정 오류안나는지 테스트필요

--- Assets/SeonMyeong/Scripts/Photon_Scene_B4.cs
+++ Assets/SeonMyeong/Scripts/Photon_Scene_B4.cs
@@ -144,7 +144,8 @@ public class Photon_Scene_B4 : MonoBehaviour
             print("Ready Count : " + inst_Ready_Count);
 
             if (inst_Ready_Count > inst_Player_Count_M)
-            {
+            {;
+                CancelInvoke("Invoke_R_89");
                 photonView_This.RPC(
                     "RPC_Order_Allow_Move_Scene_Next",
                         PhotonTargets.AllViaServer, null);
