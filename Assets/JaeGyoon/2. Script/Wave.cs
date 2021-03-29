099f44ba30b14c48c71ed3a2187eb463e806f7a8
JaeGyoon <rkdnjsxpzm@gmail.com>
2021-03-29 18:27:41 +0900

    바다 텍스쳐 변경 , 바다에 건물 건설 불가 프리뷰가 보이도록 설정

---
+++ Assets/JaeGyoon/2. Script/Wave.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    Vector3 startPos;
+
+    public float waveSpeed;
+    public float wavePower;
+
+    private void Awake()
+    {
+        startPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = gameObject.transform.position;
+
+        float theta = startPos.x + Time.time;
+        float sin = (wavePower) * Mathf.Sin(theta);
+        float cos = (waveSpeed) * Mathf.Cos(theta);
+
+        pos.x = startPos.x + cos;
+        pos.z = startPos.z + sin;
+        gameObject.transform.position = pos;
+
+
+
+    }
+}
